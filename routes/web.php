7d1e87c12496cb27ba20bb00ef7865bb4936c551
Gökhan Çelebi <dinatopya03@gmail.com>
2025-03-08 00:42:51 +0300

    d

--- routes/web.php
+++ routes/web.php
@@ -10,30 +10,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware('guest')->group(function () {
-    Route::get('login', [LoginController::class, 'create'])->name('login');
-    Route::post('login', [LoginController::class, 'store']);
-    Route::get('register', [RegisterController::class, 'create'])->name('register');
-    Route::post('register', [RegisterController::class, 'store']);
-});
-
-Route::middleware('auth')->group(function () {
-    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
-    Route::get('profile', [ProfileController::class, 'edit'])->name('profile');
-    Route::patch('profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::put('password', [ProfileController::class, 'password'])->name('password.update');
-    Route::post('logout', [LoginController::class, 'destroy'])->name('logout');
-});
-
-// Password Reset Routes
-Route::get('forgot-password', [App\Http\Controllers\Auth\ForgotPasswordController::class, 'showLinkRequestForm'])
-    ->name('password.request');
-
-Route::post('forgot-password', [App\Http\Controllers\Auth\ForgotPasswordController::class, 'sendResetLinkEmail'])
-    ->name('password.email');
-
-Route::get('reset-password/{token}', [App\Http\Controllers\Auth\ResetPasswordController::class, 'showResetForm'])
-    ->name('password.reset');
-
-Route::post('reset-password', [App\Http\Controllers\Auth\ResetPasswordController::class, 'reset'])
-    ->name('password.update');
+require __DIR__ . '/auth.php';
